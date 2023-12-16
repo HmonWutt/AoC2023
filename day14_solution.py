344e352b69fe0fb1f97e3ff083352be9eef9a386
hmon <wu2504hm-s@student.lu.se>
2023-12-16 20:25:11 +0100

    part one works

--- day14_solution.py
+++ day14_solution.py
@@ -5,20 +5,24 @@ row_max = len(file)
 col_max = len(file[0])
 
 
-def checkabove(row,col):
+def roll_north(row,col):
     while 0<=row-1<= row_max-1 and file[row-1][col] == '.':
         file[row][col] = "."
         row = row -1
-    
     file[row][col] = 'O'
            
 def replace():
      for row_ind in range(row_max):
         for col_ind in range(col_max):
             if file[row_ind][col_ind] == "O":
-                checkabove(row_ind,col_ind)
+                roll_north(row_ind,col_ind)
 
 
 replace()
-        
-print(file)
+load = row_max
+total_load = 0
+for row in file:
+    row_load = row.count("O") *load
+    load-=1
+    total_load+=row_load
+print("part one answer: ",total_load)

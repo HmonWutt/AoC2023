a42324695c5d1de0f255a1c0d26cb6f89fc61deb
hmon <wu2504hm-s@student.lu.se>
2023-12-18 00:58:52 +0100

    part two works

--- day15_solution.py
+++ day15_solution.py
@@ -1,27 +1,64 @@
 import tqdm
 with open ('day15_input.txt') as f:
     file = [i.split(',') for i in f][0]
-print(file)
+
 ascii = {}
 for i in range(0, 128):
-    #print(chr(i))
     ascii[chr(i)] = i
-
+print(file)
 def find_total(string):
     total = 0
     tmp_total = 0
-    # for each_string in file:
-    #     print(each_string)
     for each_letter in string:
-        #print(each_letter)
         times17 = (total+ascii[each_letter]) *17
         tmp_total = times17 % 256
-        #print("total, tmp_total, times17",total, tmp_total, times17)
         total =tmp_total
     return total
 total_1 = 0
 for string in tqdm.tqdm(file):
-    #print(string)
     tmp = find_total(string)
     total_1+=tmp
 print("part one answer: ", total_1)
+print("")
+boxes = {}
+for strng in tqdm.tqdm(file):
+    if strng[-2] == '=':
+        letter = strng.split('=')[0]
+        number = strng.split('=')[1]
+       
+        if find_total(letter) not in boxes:
+            boxes[find_total(letter)] = [letter+ " " + number]
+       
+        else:
+            box = [i.split(' ')[0] for i in boxes[find_total(letter)]]
+               
+            if letter in box:
+                match_ind = box.index(letter)
+                boxes[find_total(letter)][match_ind]= letter+ " " + number
+                
+                    #çprint("here", strng, boxes,boxes[find_total(letter)])
+            else:
+                boxes[find_total(letter)].append(letter+ " " + number)
+
+    else:
+    
+        letter = strng.split('-')[0]
+        number = strng.split('-')[1]
+        if find_total(letter) in boxes:
+            box = boxes[find_total(letter)]
+        
+            for each in box:    
+                if letter == each.split(' ')[0]:
+                    boxes[find_total(letter)].remove(each)
+       
+#print("final",boxes)
+total_2 = 0
+for box_num,values in boxes.items():
+    if values:
+        for slot,value in enumerate(values):
+            focal_length = value.split(' ')[1]
+            focusing_power = (box_num+1)*(slot+1) * int(focal_length)
+            total_2+=focusing_power
+
+
+print("part two answer:  ",total_2)

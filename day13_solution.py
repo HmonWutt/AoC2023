c1bd7c8d0c0b0fcd56d70bf30aebfe5a930d8e0f
hmon <wu2504hm-s@student.lu.se>
2023-12-27 12:09:14 +0100

    part one wroks

--- day13_solution.py
+++ day13_solution.py
@@ -1,16 +1,19 @@
 import numpy as np
-f =  open('day13_test.txt','r').read()
+import pandas as pd
+f =  open('day13_input.txt','r').read()
 original_file =  f.split('\n')
 
 tmp = []
 original_split = []
 for each_row in original_file:
-    if each_row != '':
+
+    if len(each_row) > 0:
         tmp.append(each_row)
+      
     else:
         original_split.append(tmp)
         tmp=[]
-#print(original_split)
+
 def transposer(file):
     tmp = [[a for a in b] for b in file]
     transposed  = [''.join(list(j)) for j in np.transpose(tmp)]
@@ -18,57 +21,46 @@ def transposer(file):
 
 
 def find_reflection_point( file):
-
     for i in range(len(file)-1):
-
         if file[i] == file[i+1]:
-            print("reflction point",i,i+1)
+            if check_if_reflection(i,i+1,file)==True:
+                return i,i+1
 
-            return i,i+1
-
-            
-def check_if_reflection(current,next,file):
-   
-    while current-1 >=0 and next+1 <= len(file)-1:
     
+def check_if_reflection(current,next,file):
+    count = 0
+    while current-1 >=0 and next+1 <= len(file)-1:
         if file[current-1] != file[next+1]:
-            return False
-        current -=1
-        next +=1       
-    return current,next
-            
+            return  False 
+        else:
+            current -=1
+            next +=1  
+            count+=1  
+    return True
+   
             
 def calculate_result (file):
     result = 0
-    current,next = find_reflection_point(file)
-    
-    if check_if_reflection(current,next, file)==False:
-       
-        current, next = find_reflection_point(transposer(file))
-        
-        result = current +1
 
+    if find_reflection_point(file) :
+        current, next = find_reflection_point( file)
+        result = (current+1) *100
+        return result
+       
     else:
-       
-        
-        row = current+1
-        result = row*100
-
-    return result
-
-
-    
+        current, next = find_reflection_point(transposer(file))
+        result = current+1
+        return result
+  
 total = 0
 
-for each in original_split:
-    print("each",each)
+for ind,each in enumerate(original_split[:-1]):
     result = calculate_result(each)
+
     total+=result
 print("PART ONE ANSWER: ", total)
    
 
 
-
-        
 
 

3723b4ff5eb2a64dbc6e550b4ed5f272bec49042
hmon <wu2504hm-s@student.lu.se>
2023-12-28 17:46:13 +0100

    part two works

--- day10_solution.py
+++ day10_solution.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 import sys
-sys.setrecursionlimit(100000) # fill function is not gentle
+sys.setrecursionlimit(1000000000) # fill function is not gentle
 with open('day10_input.txt') as f:
     directions =  [[j for j in i.rstrip()] for i in f]
 
-row_max = len(directions)-1
-col_max= len(directions[0])-1
+row_max = len(directions)
+col_max= len(directions[0])
 
 class Node:
     def __init__(self,name, row,col ):
@@ -14,17 +14,17 @@ class Node:
         self.col = col
     
     def findNeighbourLeft(self,graph):
-        if 0<= self.row <= row_max  and 0<=self.col-1<=col_max:
+        if 0<= self.row <= row_max-1  and 0<=self.col-1<=col_max-1:
             return graph[self.row][self.col-1]
          
     def findNeighbourRight(self,graph):
-        if 0<= self.row <= row_max  and 0<=self.col+1<=col_max :
+        if 0<= self.row <= row_max-1  and 0<=self.col+1<=col_max-1 :
             return  graph[self.row][self.col+1]
     def findNeighbourUp(self,graph):     
-        if 0<= self.row -1 <= row_max and 0<=self.col<=col_max:
+        if 0<= self.row -1 <= row_max-1 and 0<=self.col<=col_max-1:
             return graph[self.row-1][self.col]
     def findNeighbourDown(self,graph):     
-        if 0<= self.row +1 <= row_max  and 0<=self.col<=col_max :
+        if 0<= self.row +1 <= row_max -1 and 0<=self.col<=col_max -1:
             return graph[self.row+1][self.col]
         
     def getAllNeighbours(self,graph):
@@ -127,24 +127,126 @@ for row_ind,node in enumerate(node_graph): #########find starting point#########
     for each in node:
         if each.name == "S":
             start = each
-       
+   
 visited = set()
 visited.add(start) 
-def dfs(graph, visited, destination): 
-
+def dfs(graph, destination,visited): 
+    
     for neighbour in destination.getConnectedNeighbours(graph):  
 
         if neighbour not in visited: 
-            visited.add(neighbour)          
-            dfs(graph,visited,neighbour)
+            visited.add(neighbour)
+              
+            dfs(graph,neighbour,visited)
 
-dfs(node_graph, visited,start)
 
-
+dfs(node_graph, start,visited)
 print("PART ONE ANSWER: ",len(visited)//2)
 
 
+def flood_fill(graph, row,col): 
+    row_max = len(graph)-1
+    col_max = len(graph[0])-1
+    old_symbol = '.'
+    new_symbol = "O"
+    queue = [(row,col)]
+    while queue:
+        row,col = queue.pop(0)
+        if (row <0 or row >row_max) or (col <0 or col>col_max) or graph[row][col] !=old_symbol:
+            continue
+        else:
+            graph[row][col] = new_symbol
+            flood_fill(graph,row,col-1)
+            flood_fill(graph,row,col+1)
+            flood_fill(graph,row-1,col)
+            flood_fill(graph,row+1,col)
 
-            
+board = [["." for m in range(3*col_max)] for n in range(row_max*3)]    
 
+def mark_border(board,node,directions):
+    row_num = node.row
+    col_num = node.col
+    middle_row = 3*row_num +1
+    middle_col = 3*col_num +1
+    if directions[row_num][col_num]=='-':
+
+        board[middle_row][middle_col] = "#"
+        board[middle_row][middle_col-1]= '#'
+        board[middle_row][middle_col+1] = "#"
+
+    if directions[row_num][col_num]=='|':
+        
+        board[middle_row][middle_col] = "#"
+        board[middle_row-1][middle_col]='#'
+        board[middle_row+1][middle_col] = "#"
+
+    if directions[row_num][col_num]=='L':
+        
+        board[middle_row][middle_col] = "#"
+        board[middle_row-1][middle_col]='#'
+        board[middle_row][middle_col+1] = "#"
+    
+    if directions[row_num][col_num]=='J':
+        
+        board[middle_row][middle_col] = "#"
+        board[middle_row-1][middle_col]='#'
+        board[middle_row][middle_col-1] = "#"
+    if directions[row_num][col_num]=='7':
+        
+        board[middle_row][middle_col] = "#"
+        board[middle_row+1][middle_col]='#'
+        board[middle_row][middle_col-1] = "#"
+
+    if directions[row_num][col_num]=='F':
+        
+        board[middle_row][middle_col] = "#"
+        board[middle_row+1][middle_col]='#'
+        board[middle_row][middle_col+1] = "#"
+    if directions[row_num][col_num] =="S":
+        
+        board[middle_row][middle_col] = "#"
+        board[middle_row+1][middle_col]='#'
+        board[middle_row][middle_col-1] = "#"
+
+    
+
+def flood_fill_board(graph, row,col): 
+    row_max = len(graph)-1
+    col_max = len(graph[0])-1
+    old_symbol = '.'
+    new_symbol = "O"
+    queue = [(row,col)]
+    while queue:
+        row,col = queue.pop(0)
+      
+        if (row <0 or row >row_max) or (col <0 or col>col_max) or graph[row][col] !=old_symbol:
+            continue
+        else:
+            graph[row][col] = new_symbol
+            queue.append((row,col-1))
+            queue.append((row,col+1))
+            queue.append((row-1,col))
+            queue.append((row+1,col))
+
+for e_node in visited:
+    mark_border(board,e_node,directions)
+
+flood_fill_board(board,0,0)
+count = 0
+for row_num in range(0,len(board),3):
+    for col_num in range(0,len(board[0]),3):
+        middle_row= row_num+1
+        middle_col = col_num+1
+
+        true_counts =0
+        for row in range(middle_row-1,middle_row+2):
+            for col in range(middle_col-1, middle_col+2):
+                if board[row][col] == '.':
+                    true_counts+=1
+                   
+        if true_counts == 9:
+            count+=1
+                       
+print("PART TWO ANSWER: ",count)
+newboard = ["".join(i) for i in board]
 

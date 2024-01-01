eabe30e3c760b8b56049223986ab53fb48f19a29
hmon <wu2504hm-s@student.lu.se>
2024-01-01 20:16:47 +0100

    part two works

--- day13_solution.py
+++ day13_solution.py
@@ -29,6 +29,7 @@ def find_reflection_point( file):
     
 def check_if_reflection(current,next,file):
     count = 0
+    
     while current-1 >=0 and next+1 <= len(file)-1:
         if file[current-1] != file[next+1]:
             return  False 
@@ -59,8 +60,72 @@ for ind,each in enumerate(original_split[:-1]):
 
     total+=result
 print("PART ONE ANSWER: ", total)
-   
 
 
 
+def find_smudge(current_ind, next_ind, file):
+    difference = 0
+    current = file[current_ind]
+    next = file[next_ind]
+    for i in range(len(current)):
+        if difference > 1:
+            break
+        if current[i] != next[i]:
+            difference+=1
+    return difference
 
+
+def count_smudge(current, next, file):
+    smudge = 0
+    while current >= 0 and next <= len(file)-2:
+        if find_smudge(current,next,file) >=2:
+            break
+        if find_smudge(current, next,file) == 0:
+            current-=1
+            next+=1
+
+        if find_smudge(current, next,file) == 1:
+            smudge+=1
+            current-=1
+            next+=1
+    return smudge
+
+
+def check_if_one_smudge(file):
+    for i in range(len(file)-1):
+        current = i
+        next = i+1
+        if find_smudge(current, next, file) == 1 and check_if_reflection(current,next,file) == True:
+            return current,next        
+        if file[current]== file[next] and count_smudge(current, next, file) == 1:
+            return current,next
+    return -1
+        
+        
+        
+
+total_2 =0           
+for ind,file in enumerate(original_split[:-1]):
+    print("id-------------------,",ind,len(file))
+    if check_if_one_smudge(file) != -1:
+        current, _ = check_if_one_smudge(file)
+        result = (current+1)*100
+
+    else:
+        if check_if_one_smudge(transposer(file)) == -1:
+            print("This should never happen", ind, check_if_one_smudge(transposer(file)))
+            for i in file:
+                print(i)
+
+            print('')
+            for i in transposer(file):
+                print(i)
+        else:
+            current,_ = check_if_one_smudge(transposer(file))
+            result = current+1
+    total_2+=result
+print("part two answer:  ", total_2)
+
+
+
+ 
